8cdc46dd688b15caca38e699d78ae0ea054764fa
Ali Zaidi <alikaz.zaidi@gmail.com>
2017-09-21 01:40:18 -0400

    add script driven install

---
+++ Students/dsvm/lab/2-updating-CNTK/cntk-install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# create conda environment and install cntk
+conda create -n cntk-py35 python=3.5 anaconda
+source activate cntk-py35
+pip install https://cntk.ai/PythonWheel/GPU/cntk-2.2-cp35-cp35m-linux_x86_64.whl
+
+# take ownership of root anaconda
+sudo chown $whoami:$whoami -R /anaconda/
+
+# update root anaconda python and jupyter
+source deactivate
+pip install -U pip
+pip install --upgrade --force-reinstall jupyter
+rm ~/.ipython/profile_default/startup/az_ml_magic.py
+
+
+# update conda kernels
+conda install nb_conda
+conda install ipykernel
+python -m ipykernel install --user --name cntk-py35 --display-name "cntk-py35"
+jupyter kernelspec list
+
+# install keras and tensorflow-gpu
+source activate cntk-py35
+pip install keras
+pip install tensorflow-gpu

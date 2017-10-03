6b1d288269a0b4f2b2c2cd492524e2646f920df3
Ali Zaidi <alikaz.zaidi@gmail.com>
2017-10-03 13:55:17 -0700

    update disk size with parameterization of osdisk

--- Students/dsvm/lab/1-update-disk-size-script/expand-osdisk.sh
+++ Students/dsvm/lab/1-update-disk-size-script/expand-osdisk.sh
@@ -1,24 +1,19 @@
 #!/usr/bin/env bash
 #title           :deploydsvm.sh
 #description     :This script creates updates disk size on a Linux DSVM in Azure.
-#author		 	 :Ali Zaidi (github: akzaidi; contact alizaidi at microsoft dot com)
+#author	         :Ali Zaidi (github: akzaidi; contact alizaidi at microsoft dot com)
 #date            :2017-08-04
 #version         :0.1    
-#usage		 	 :bash expand-osdisk.sh 
+#usage           :bash expand-osdisk.sh
 #notes           :Requires azure-CLI, and you must login prior to usage, az login.
 #====================================================================================
 
-yourname=$(whoami)
-class="dlclass"
-vmname="dsvm"
+OSSIZE=${1:-200}
+RG=$2
+VMNAME=$3
 
-RG=$yourname$class
-
-VMNAME=$yourname$vmname
-
-OSDISK=$1
-OSSIZE=${2:-200}
-
+# Get Disk Name
+read OSDISK disksize <<< $(./get-osdisk-name.sh $RG | grep "Os")
 
 # Deallocate so resizing can be done
 
